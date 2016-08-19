62efa3becf917d6a91b55924fae0b450f0dd5497
Stephen Lee <stephenslee0127@gmail.com>
2016-08-19 07:43:26 +0000

    Users cannot login without activation link verified.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)   #check the paramater. if true, remember. If false, forget the cookie.
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)   #check the paramater. if true, remember. If false, forget the cookie.
+        redirect_back_or user
+      else
+        message = "account not activated. "
+        message += "check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+      
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- test/controllers/account_activations_controller_test.rb
+++ test/controllers/account_activations_controller_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
 
 class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
 end
