9983a1ad99aaa455566adc8c08e52d8f7ca6ce95
Stephen Lee <stephenslee0127@gmail.com>
2016-08-23 06:00:37 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://rails-pratice-stephen42.c9users.io.herokuapp.com'
+  host = 'desolate-plateau-53347.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
